4d1638f6d16b5daaa2172259ba49322b580c249e
ahmad jafari <ahmad.mrj@gmail.com>
2020-07-13 17:12:59 +0430

    media bug fixed.

--- app/Http/Controllers/Admin/CartridgeCrudController.php
+++ app/Http/Controllers/Admin/CartridgeCrudController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Requests\CartridgeRequest;
-use App\models\CartridgeMedia;
+use App\Models\CartridgeMedia;
 use Backpack\CRUD\app\Http\Controllers\CrudController;
 use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
 use Illuminate\Http\Request;

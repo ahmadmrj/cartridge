e048a778709c196861ffc1ceb75f5998c79263bf
ahmad jafari <ahmad.mrj@gmail.com>
2020-08-30 11:46:41 +0430

    categories refined.

--- app/Http/Controllers/API/CategoryController.php
+++ app/Http/Controllers/API/CategoryController.php
@@ -18,14 +18,41 @@ class CategoryController extends Controller
         $res = \DB::connection('shop')->select(\DB::raw('
             SELECT
             pts_category.id_category,
-            pts_category_lang.name AS cat
+            pts_category_lang.name AS cat,
+            catgroup.id_category AS group_id,
+            catgroup_lang.name AS group_name
             FROM pts_category
             INNER JOIN pts_category_lang ON pts_category_lang.id_category = pts_category.id_category
+            INNER JOIN pts_category catgroup ON catgroup.id_parent = pts_category.id_category
+            INNER JOIN pts_category_lang catgroup_lang ON catgroup_lang.id_category = catgroup.id_category
             WHERE
-            pts_category.level_depth = 2 AND pts_category.id_category < 200
+            pts_category.level_depth = 2
+            AND pts_category.id_category < 200
         '));
 
-        return response()->json($res, 200);
+        $result = [];
+
+        foreach ($res as $item) {
+            if(!isset($result[$item->id_category])) {
+                $result[$item->id_category] = new \stdClass();
+                $result[$item->id_category]->name = $item->cat;
+                $result[$item->id_category]->id = $item->id_category;
+                $result[$item->id_category]->children = [];
+            }
+
+            array_push($result[$item->id_category]->children, [
+                'group_name' => $item->group_name,
+                'group_id' => $item->group_id
+            ]);
+        }
+
+        $finalRes = [];
+
+        foreach ($result as $value) {
+            $finalRes[] = $value;
+        }
+
+        return response()->json($finalRes, 200);
     }
 
     /**

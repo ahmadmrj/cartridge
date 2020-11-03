50f26761cc8bca451a80eedd8078ea884bd7e542
ahmad jafari <ahmad.mrj@gmail.com>
2020-11-03 18:59:21 +0330

    quantity updater refined.

--- app/Services/ShopUpdater/QuantityUpdater.php
+++ app/Services/ShopUpdater/QuantityUpdater.php
@@ -13,23 +13,32 @@ class QuantityUpdater extends ShopUpdaterGeneral implements ShopUpdater {
     public function update()
     {
         if($this->attribute) {
+            $shopStock = ShopStockAvailable::where('id_product', $this->attribute->id_product)
+                ->where('id_product_attribute', $this->attribute->id_product_attribute)
+                ->first();
+
+            $shopStock->quantity = $this->value;
+            $shopStock->save(['timestamps' => false]);
+
+            // Fix sumation
+            $shopStockSum = ShopStockAvailable::select(\DB::raw('SUM(quantity) as total_quantity'))
+                ->where('id_product', $this->attribute->id_product)
+                ->where('id_product_attribute', '!=',0)
+                ->first();
+
             $shopStockBase = ShopStockAvailable::where('id_product', $this->attribute->id_product)
                 ->where('id_product_attribute', 0)
                 ->first();
 
-            $shopStockBase->quantity = $shopStockBase->quantity + 1;
+            $shopStockBase->quantity = $shopStockSum->total_quantity;
             $shopStockBase->save();
-
-            $shopStock = ShopStockAvailable::where('id_product', $this->attribute->id_product)
-                ->where('id_product_attribute', $this->attribute->id_product_attribute)
-                ->first();
         } else {
             $shopStock = ShopStockAvailable::where('id_product', $this->product->id_product)->first();
             $this->product->quantity = $this->value;
             $this->product->save(['timestamps' => false]);
-        }
 
-        $shopStock->quantity = $this->value;
-        $shopStock->save(['timestamps' => false]);
+            $shopStock->quantity = $this->value;
+            $shopStock->save(['timestamps' => false]);
+        }
     }
 }
